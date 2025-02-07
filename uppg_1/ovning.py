c23b07dac387b7f4d5b0b0fc679754f1ebc1ea5b
Calico-Katt <egrahn03@gmail.com>
2025-02-07 09:30:01 +0100

    Har gjort projektion med 2 vectorer
    Koden är ej testad men ser ut att vara rätt.

--- uppg_1/ovning.py
+++ uppg_1/ovning.py
@@ -3,30 +3,36 @@ import numpy as np
 
 class Vector:
 
-    def __init__(self, vector1, vector2, normalvector):
+    def __init__(self, vector1, vector2):
         self.v1 = vector1
         self.v2 = vector2
-        self.nv = normalvector
+
+    def riktadevectorer(self):
+        summa = []
+        for i in range(self.v1):
+            summa.append((self.v1[i]*self.v2[i])/(self.v2[i]**2))
+        #for-loopen går igenom varje skalär i de två vektrorerna och utför projektionens ekvation
+        return summa
     
 
 def interaction():
-    #Här kollar vi vad de tänker skriva in så att programmet inte klagar
+    # Här kollar vi vad de tänker skriva in så att programmet inte klagar
     svar = input("Tänker du använda två riktningsvektorer eller en normalvector? Tryck 1 för normalvecktor och 2 för riktningsvektorer.")
     print (svar)
 
     if svar == 1:
-        nyVector=input("Skriv in din normalvector")
+        nyVector = input("Skriv in din normalvector")
         pass
 
     elif svar == 2:
-        Vector1=input("Skriv in din första vector")
+        Vector1 = input("Skriv in din första vector")
         Vector2 = input("Skriv in din andra vector")
-        pass
+        Vector.riktadevectorer(Vector1, Vector2)
 
     else:
         print("Du valde inte rätt, hejdå!")
 
 
 if __name__ == "__main__":
-    pass
+    interaction()
 

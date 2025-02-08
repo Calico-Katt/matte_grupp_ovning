a79117a2cc1c2090f4e4eceebaf0c3e6da88fe37
Mille <182352340+Mille-code9@users.noreply.github.com>
2025-02-08 14:06:14 +0100

    Update test.py

--- uppg_1/test.py
+++ uppg_1/test.py
@@ -1,12 +1,13 @@
 import numpy as np
 
+#Need to send in p, v1 & v2 or n, and also method. v1, v2, n and p needs to be in list-types to work
 def projectPointToPlan(p, method = "direction", v1=None, v2=None, n=None):
     p_array = np.array(p)
     if method == "direction":
         #Need to input a interaction function that asks for the two vectors and check so the list have 3 positions. Value error might need to be moved to interaction.
         if v1 is None or v2 is None:
             raise ValueError("Two direction vectors needs too be given for the method 'direction'")
-        v1_array, v2_array = np.array(v1), np.array(v2) #Think it might be easier to make the vectors in to arrays instead of list because of the functions in np.
+        v1_array, v2_array = np.array(v1), np.array(v2) #Turn list into array gives easier acess to np functions
         #solve ekvation not sure this is rights need more time to check
         a = np.array([[np.dot(v1_array, v1_array), np.dot(v1_array,v2_array)],
                       [np.dot(v2_array, v1_array), np.dot(v2_array, v2_array)]])
@@ -22,8 +23,10 @@ def projectPointToPlan(p, method = "direction", v1=None, v2=None, n=None):
         #Sovle scalar projection (i think I'm really not sure but is making the ekvation in the book in to code)
         u = np.dot(p_array, n_array) / np.dot(n_array, n_array)
         p_projection = p_array - u * n_array
+
     else:
         raise ValueError("Method must be 'direction' or 'normal'")
+    
     #Depending on which if function is activated, the program will give the projection of the asked ekvation
     return p_projection 
 

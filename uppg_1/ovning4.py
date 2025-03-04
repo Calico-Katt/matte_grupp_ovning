91cb44a930ac2fe3f9154fbebb82706a63da80db
Calico-Katt <egrahn03@gmail.com>
2025-03-04 18:12:05 +0100

    Upp A ovning 4
    Tror jag har löst uppg. A

--- uppg_1/ovning4.py
+++ uppg_1/ovning4.py
@@ -3,3 +3,19 @@ from scipy import linalg as LA
 from sympy import Matrix
 import time
 
+A = Matrix([[1, 2, 1, -1, 2],
+              [2, 4, 5, 2, 0],
+              [2, 2, 1, 0, 2]])
+
+
+def uppA():
+    # Skapar en matris på formen [A|0]
+    totA = A.row_join(Matrix([[0], [0], [0]]))
+
+    # Löser systemet med Gausseliminering (rref)
+    rref_matrix, pivot_columns = totA.rref()
+
+    print("Reducerad matris:")
+    print(rref_matrix)
+
+uppA()

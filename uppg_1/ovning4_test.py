f2733d66ce9b8d230dd7ef8e1fd9b7f4171dc71e
Mille <182352340+Mille-code9@users.noreply.github.com>
2025-03-05 11:51:34 +0100

    Uppgift A
    Compleat solution for uppgift A. not the most effectiv but it works

--- uppg_1/ovning4_test.py
+++ uppg_1/ovning4_test.py
@@ -1,23 +1,35 @@
 import numpy as np
 from scipy import linalg as LA
-from sympy import Matrix
+from sympy import Matrix, symbols
 import time
 
 def rref(A):
     rref_matrix, pivot_columns = A.rref()
     print(f"Radreducerad matris:\n{rref_matrix}\nPivotkolonner: {pivot_columns}")
-    free_variables_columns = [i for i in range(A.shape[1]) if i not in pivot_columns]
+    total_columns = A.shape[1]
+    free_variables_columns = [i for i in range(total_columns) if i not in pivot_columns]
     solutions = rref_matrix.nullspace()
-    "Need to add the free variables with t, s, v... so the solution makes more seens, and check so the positiv and negativ works"
+    free_symbols = symbols("t s v w z")[:len(free_variables_columns)]
+    free_variables_symbol = {f"x{var+1}": sym for var, sym in zip(free_variables_columns, free_symbols)}
+    x_solutions = Matrix.zeros(total_columns, 1)
     if solutions:
-        print()
-        for idx, sol in enumerate(solutions):
-            print(f"x_{idx} = {sol}")
+        print("Fria variabler:")
+        for var, sym in free_variables_symbol.items():
+            print(f"{var} = {sym}")
+        print(f"Där {', '.join(map(str, free_symbols))} ∈ ℝ.\n")
+        print("Lösning till Ax = 0")
+        for free_variables, symbol, solution in zip(free_variables_columns, free_symbols, solutions):
+                x_solutions += symbol * solution
+        print(f"x = {x_solutions}\n")
+
+def kl():
+    pass
 
 def uppgiftA():
     A = Matrix([[1, 2, 1, -1, 2],
                 [3, 4, 5, 2, 0],
                 [2, 2, 1, 0, 2]])
+    print("\nUppgift A\n")
     rref(A)
 
 
